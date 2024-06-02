c1bc6153045e274372a735c83a0405acf65841cf
Lucas Alves <lucasalves@pop-os.localdomain>
2024-06-02 11:48:39 -0300

    COnfigurações rabbit
    Rabbit integrado ao sistema

--- config/queue.php
+++ config/queue.php
@@ -71,6 +71,20 @@ return [
             'after_commit' => false,
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', 'rabbitmq'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'user'),
+                    'password' => env('RABBITMQ_PASSWORD', 'password'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ]
+        ],
+
+
     ],
 
     /*
--- tests/Feature/RabbitTest.php
+++ tests/Feature/RabbitTest.php
@@ -2,19 +2,31 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
+use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Connectors\RabbitMQConnector;
 
 class RabbitTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function testEnvioDeMensagemParaFila(): void
     {
-        $response = $this->get('/');
+        $mensagem = ["have" => "valor"];
 
-        $response->assertStatus(200);
+        Queue::connection("rabbitmq")->pushRaw(json_encode($mensagem), "nome_da_fila");
+
+        $this->assertTrue(true);
     }
+
+    public function testConsumirFila()
+    {
+        $result = Queue::connection("rabbitmq")->pop("nome_da_fila");
+
+        Log::info($result->getRawBody());
+
+    }
+
 }
